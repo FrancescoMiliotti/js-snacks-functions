399d4921952d53bd5cf891bd03bb3ea3c72530e8
francesco.miliotti <francesco.miliotti@email.com>
2025-11-10 16:24:31 +0100

    esercizio conta vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,6 +5,34 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+const contaVocali = (stringa) => {
+    const vocali = [ "a","e","i","o","u"]
+    let result = 0
+
+    for (let i = 0; i < stringa.length; i++) {
+        const lettera = stringa.charAt(i).toLowerCase();
+
+        if ( vocali.includes(lettera)){
+            result++;
+        }
+        
+    }
+    return result;
+}
+
+    
+
+    console.log(contaVocali(word));
+
+    
+
+        
+
+
+
+
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
