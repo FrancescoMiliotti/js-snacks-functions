1777c4f32e9512d140a8cc31d597636208df7962
francesco.miliotti <francesco.miliotti@email.com>
2025-11-10 16:04:49 +0100

    esercizio filtra iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,38 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const filtraIniziale = (array, letter) => {
+    const result = []
+    const letteraDaControllare = letter.toLowerCase()
+
+    for (let i = 0; i < array.length; i++) {
+        const parola = array[i];
+        const iniziale = parola[0].toLowerCase();
+
+        console.log(i, parola, iniziale);
+
+        if (iniziale === letter) {
+            result.push(parola);
+
+        }
+
+        console.log("Stato attuale array", i, result);
+
+
+
+
+    }
+
+    return result;
+
+}
+
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
+let inizialiParole = filtraIniziale(names, "a");
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+console.log("Risultato finale", inizialiParole);
