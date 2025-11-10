976f7958e3720856de838343079a9edf3685d3b2
francesco.miliotti <francesco.miliotti@email.com>
2025-11-10 15:37:43 +0100

    esercizio iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,29 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function inizialiNomi(array) {
+    let lettereiniziali = []
+
+    for (let i = 0; i < array.length; i++) {
+        console.log(i,lettereiniziali)
+
+        const primoCarattere = array[i][0];
+        console.log(primoCarattere)
+        lettereiniziali.push(primoCarattere);
+    }
+
+    return lettereiniziali;
+
+}
+
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+let initials = inizialiNomi(names)
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+console.log(initials)
